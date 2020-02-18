4f59e4652b8d6a33f8068e31d59b9d0e7d2780cb
Halle Vogelpohl <hvogelpo@lion.lmu.edu>
2020-02-17 21:56:35 -0800

    removed js file

--- index.js
+++
@@ -1,7 +0,0 @@
-const home = document.querySelector("#home");
-const about = document.querySelector("#about");
-const linkedin = document.querySelector("#linkedin");
-const github = document.querySelector("#github");
-const vsco = document.querySelector("#vsco");
-const ig = document.querySelector("#ig");
-const book = document.querySelector("#book");
